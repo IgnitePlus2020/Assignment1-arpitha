e6b62cb3b454570cfe88864823d17ac9ad5dbfa8
JOIS <arpitha6556@gmail.com>
2020-03-19 23:16:34 +0530

    ok

--- src/com/tgt/igniteplus/Challenge1.java
+++ src/com/tgt/igniteplus/Challenge1.java
@@ -1,4 +1,14 @@
+//Challenge 1:A program to display current Date and Time
+
 package com.tgt.igniteplus;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Challenge1 {
+    public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("Current date and time is: "+dtf.format(now));
+    }
 }
